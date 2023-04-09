70a765566982f753df051a112ac4501eb3384689
Andrei <judo73228@gmail.com>
2023-04-09 22:30:35 +0400

    end

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const modal = document.querySelector('.pop-up');
+
+
+function openModal(){
+    modal.style.display = "flex";
+}
+function closeModal(){
+    modal.style.display = "none";
+}
+function validationСheck(){
+    const inputName = document.querySelector('.name-input').value;
+    const inputEmail = document.querySelector('.email-input-sub').value;
+    const inputLetter = document.querySelector('.letter-input-sub').value;
+    const borderName = document.querySelector('.name-input');
+    const borderEmail = document.querySelector('.email-input-sub');
+    const borderLetter = document.querySelector('.letter-input-sub');
+    
+}
